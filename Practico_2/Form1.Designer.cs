ce55e0122fb78fe7c87c606d4899db4ed7d39e69
naza <mendeznazareno27@gmail.com>
2025-08-16 03:12:07 -0300

    Pequeño Formulario

--- Practico_2/Form1.Designer.cs
+++ Practico_2/Form1.Designer.cs
@@ -28,37 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.botonGuardar = new System.Windows.Forms.Button();
-            this.Boton_Eliminar = new System.Windows.Forms.Button();
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.tEliminar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TNombre = new System.Windows.Forms.TextBox();
+            this.tApellido = new System.Windows.Forms.TextBox();
             this.LNya = new System.Windows.Forms.Label();
             this.LModificar = new System.Windows.Forms.Label();
             this.LDni = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TDni = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // botonGuardar
+            // BGuardar
             // 
-            this.botonGuardar.Location = new System.Drawing.Point(26, 188);
-            this.botonGuardar.Name = "botonGuardar";
-            this.botonGuardar.Size = new System.Drawing.Size(62, 26);
-            this.botonGuardar.TabIndex = 0;
-            this.botonGuardar.Text = "Guardar";
-            this.botonGuardar.UseVisualStyleBackColor = true;
-            this.botonGuardar.Click += new System.EventHandler(this.button1_Click);
+            this.BGuardar.Location = new System.Drawing.Point(26, 188);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(62, 26);
+            this.BGuardar.TabIndex = 0;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.UseVisualStyleBackColor = true;
+            this.BGuardar.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Boton_Eliminar
+            // tEliminar
             // 
-            this.Boton_Eliminar.Location = new System.Drawing.Point(129, 188);
-            this.Boton_Eliminar.Name = "Boton_Eliminar";
-            this.Boton_Eliminar.Size = new System.Drawing.Size(62, 26);
-            this.Boton_Eliminar.TabIndex = 1;
-            this.Boton_Eliminar.Text = "Eliminar";
-            this.Boton_Eliminar.UseVisualStyleBackColor = true;
-            this.Boton_Eliminar.Click += new System.EventHandler(this.button2_Click);
+            this.tEliminar.Location = new System.Drawing.Point(129, 188);
+            this.tEliminar.Name = "tEliminar";
+            this.tEliminar.Size = new System.Drawing.Size(62, 26);
+            this.tEliminar.TabIndex = 1;
+            this.tEliminar.Text = "Eliminar";
+            this.tEliminar.UseVisualStyleBackColor = true;
+            this.tEliminar.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -79,21 +79,25 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Apellido";
             // 
-            // textBox1
+            // TNombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(93, 100);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(98, 22);
-            this.textBox1.TabIndex = 4;
+            this.TNombre.Location = new System.Drawing.Point(93, 100);
+            this.TNombre.Multiline = true;
+            this.TNombre.Name = "TNombre";
+            this.TNombre.Size = new System.Drawing.Size(98, 22);
+            this.TNombre.TabIndex = 4;
+            this.TNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.TNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TNombre_KeyPress);
             // 
-            // textBox2
+            // tApellido
             // 
-            this.textBox2.Location = new System.Drawing.Point(93, 140);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(98, 22);
-            this.textBox2.TabIndex = 5;
+            this.tApellido.Location = new System.Drawing.Point(93, 140);
+            this.tApellido.Multiline = true;
+            this.tApellido.Name = "tApellido";
+            this.tApellido.Size = new System.Drawing.Size(98, 22);
+            this.tApellido.TabIndex = 5;
+            this.tApellido.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.tApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tApellido_KeyPress);
             // 
             // LNya
             // 
@@ -123,30 +127,32 @@
             this.LDni.TabIndex = 9;
             this.LDni.Text = "DNI";
             // 
-            // textBox4
+            // TDni
             // 
-            this.textBox4.Location = new System.Drawing.Point(93, 64);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(98, 22);
-            this.textBox4.TabIndex = 10;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.TDni.Location = new System.Drawing.Point(93, 64);
+            this.TDni.Multiline = true;
+            this.TDni.Name = "TDni";
+            this.TDni.Size = new System.Drawing.Size(98, 22);
+            this.TDni.TabIndex = 10;
+            this.TDni.Click += new System.EventHandler(this.TDni_Click);
+            this.TDni.TextChanged += new System.EventHandler(this.TDni_TextChanged);
+            this.TDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TDni_KeyPress);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(307, 261);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.TDni);
             this.Controls.Add(this.LDni);
             this.Controls.Add(this.LModificar);
             this.Controls.Add(this.LNya);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tApellido);
+            this.Controls.Add(this.TNombre);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Boton_Eliminar);
-            this.Controls.Add(this.botonGuardar);
+            this.Controls.Add(this.tEliminar);
+            this.Controls.Add(this.BGuardar);
             this.Name = "Form1";
             this.Text = "Pequeño Formulario";
             this.ResumeLayout(false);
@@ -156,16 +162,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Button botonGuardar;
-        private System.Windows.Forms.Button Boton_Eliminar;
+        private System.Windows.Forms.Button BGuardar;
+        private System.Windows.Forms.Button tEliminar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TNombre;
+        private System.Windows.Forms.TextBox tApellido;
         private System.Windows.Forms.Label LNya;
         private System.Windows.Forms.Label LModificar;
         private System.Windows.Forms.Label LDni;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox TDni;
     }
 }
 
--- Practico_2/Form1.cs
+++ Practico_2/Form1.cs
@@ -19,7 +19,71 @@ namespace Practico_2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(TDni.Text) || string.IsNullOrEmpty(TNombre.Text) || string.IsNullOrEmpty(tApellido.Text))
+            {
+                MessageBox.Show("Debe completar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else
+            {
+                LModificar.Text = TNombre.Text + " " + tApellido.Text;
+                DialogResult dialogResult = MessageBox.Show("Seguro que desea insertar un nuevo cliente?", "Confirmar inserción", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    MessageBox.Show("El cliente: " + LModificar.Text + " se insertó correctamente", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
+                }
+
+            }
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TDni.Text) || string.IsNullOrEmpty(TNombre.Text) || string.IsNullOrEmpty(tApellido.Text))
+            {
+                MessageBox.Show("Debe completar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                LModificar.Text = TNombre.Text + " " + tApellido.Text;
+                DialogResult dialogResult = MessageBox.Show("Esta a punto de eliminar el cliente: " + LModificar.Text, "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    MessageBox.Show("El cliente " + LModificar.Text + " se eliminó correctamente", "Eliminar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    TNombre.Text = string.Empty; tApellido.Text = string.Empty;
+                    TDni.Text = string.Empty; LModificar.Text = string.Empty;
+                }
+            }
+        }
+
+        private void TDni_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Este campo solo admite números", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void TNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Este campo solo admite letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void tApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -27,14 +91,25 @@ namespace Practico_2
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e) //nombre
         {
 
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        private void textBox2_TextChanged(object sender, EventArgs e) //apellido
         {
 
         }
+
+        private void TDni_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TDni_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
