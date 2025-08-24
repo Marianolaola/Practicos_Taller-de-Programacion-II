75bef076149a7e7e8faf646570c8264ad4e52738
Marianolaola <laolamariano@gmail.com>
2025-08-24 15:20:39 -0300

    Creación y modificación del Form 4

---
+++ Práctico 4/Formulario 4.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Práctico_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerarFuncion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDesde = new System.Windows.Forms.TextBox();
+            this.txtHasta = new System.Windows.Forms.TextBox();
+            this.lista_Numeros = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerarFuncion
+            // 
+            this.btnGenerarFuncion.Location = new System.Drawing.Point(179, 222);
+            this.btnGenerarFuncion.Name = "btnGenerarFuncion";
+            this.btnGenerarFuncion.Size = new System.Drawing.Size(109, 23);
+            this.btnGenerarFuncion.TabIndex = 0;
+            this.btnGenerarFuncion.Text = "Generar Función";
+            this.btnGenerarFuncion.UseVisualStyleBackColor = true;
+            this.btnGenerarFuncion.Click += new System.EventHandler(this.btnGenerarFuncion_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Desde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hasta";
+            // 
+            // txtDesde
+            // 
+            this.txtDesde.Location = new System.Drawing.Point(81, 127);
+            this.txtDesde.Name = "txtDesde";
+            this.txtDesde.Size = new System.Drawing.Size(129, 20);
+            this.txtDesde.TabIndex = 3;
+            this.txtDesde.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDesdeKeyPress);
+            // 
+            // txtHasta
+            // 
+            this.txtHasta.Location = new System.Drawing.Point(81, 171);
+            this.txtHasta.Name = "txtHasta";
+            this.txtHasta.Size = new System.Drawing.Size(129, 20);
+            this.txtHasta.TabIndex = 4;
+            this.txtHasta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHastaKeyPress);
+            // 
+            // lista_Numeros
+            // 
+            this.lista_Numeros.FormattingEnabled = true;
+            this.lista_Numeros.Location = new System.Drawing.Point(333, 99);
+            this.lista_Numeros.Name = "lista_Numeros";
+            this.lista_Numeros.Size = new System.Drawing.Size(247, 199);
+            this.lista_Numeros.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Lista de números";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 370);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lista_Numeros);
+            this.Controls.Add(this.txtHasta);
+            this.Controls.Add(this.txtDesde);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerarFuncion);
+            this.Name = "Form1";
+            this.Text = "Formulario4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerarFuncion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDesde;
+        private System.Windows.Forms.TextBox txtHasta;
+        private System.Windows.Forms.ListBox lista_Numeros;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Práctico 4/Formulario 4.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Práctico_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDesdeKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Solo se permiten números enteros", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void txtHastaKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Solo se permiten números enteros", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnGenerarFuncion_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtDesde.Text) || string.IsNullOrEmpty(txtHasta.Text)){
+
+                MessageBox.Show("Por favor, complete ambos campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int desde = int.Parse(txtDesde.Text);
+            int hasta = int.Parse(txtHasta.Text);
+
+            if (desde > hasta)
+            {
+                MessageBox.Show("El valor de inicio debe ser mayor que el valor final", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            for (int i = desde; i <= hasta; i++)
+            {
+                lista_Numeros.Items.Add(i);
+            }
+        }
+    }
+}
